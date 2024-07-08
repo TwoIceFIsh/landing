da51bae5b108876ba15a129a7e32b47bdbab71b4
teamk <team.k0konutz@gmail.com>
2024-07-09 00:20:33 +0900

    init

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,132 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className={"flex h-full flex-col"}>
+      <div
+        className={
+          "flex h-[80px] w-full flex-row items-center justify-between bg-black bg-gradient-to-bl from-black to-purple-950 px-20"
+        }
+      >
+        <Image
+          src={"/logo.png"}
+          alt={"logo"}
+          width={60}
+          height={60}
+          className={"rounded-full opacity-80"}
+        />
+        <div className={"flex gap-6 font-bold text-white"}>
+          <div>
+            <Link href={"/"}>홈</Link>
+          </div>
+          <div>
+            <Link href={"https://www.rocketpunch.com/@ruinaway"}>프로필</Link>
+          </div>
+          <div>
+            <Link href={"https://twoicefish-secu.tistory.com"}>블로그</Link>
+          </div>
+          <div>
+            <Link href={"https://github.com/twoicefish"}>깃허브</Link>
+          </div>
+        </div>
+        <div>
+          <div>ss</div>
         </div>
       </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
+      <div className="relative">
         <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+          src={"/bg.jpg"}
+          alt={"bg"}
+          width={1920}
+          height={1080}
+          className={"w-full"}
         />
+        <div className="absolute left-20 right-20 top-20 flex flex-col items-start justify-center gap-4">
+          <div
+            className={
+              "flex w-full flex-col gap-4 rounded-lg bg-black bg-opacity-50 px-4 py-1 shadow-md"
+            }
+          >
+            <div>
+              <p className="text-3xl font-bold text-yellow-500">
+                정보보안 전문가 / 프로그래머
+              </p>
+
+              <p className="text-3xl font-bold text-white">이병호</p>
+            </div>
+            <div>
+              <p className="text-xl font-bold text-white">
+                kingelip@cyber-luna.com
+              </p>
+            </div>
+          </div>
+          <div className={"flex w-full justify-between gap-2"}>
+            <div className={"rounded-lg bg-black bg-opacity-50 p-4 shadow-md"}>
+              <p className={"font-bold text-white"}>정보보안</p>
+              <br />
+              <p className={"font-bold text-white"}>이글루시큐리티</p>
+              <p className={"text-white"}>2019.03 ~ 2020.03 : 해외기술팀</p>
+              <p className={"text-white"}>
+                2020.03 ~ 2021.03 : 가급 국가기관 사이버안전센터
+              </p>
+              <p className={"text-white"}>
+                2021.03 ~ 2022.05 : S-OIL 보안관제팀
+              </p>
+              <br />
+              <p className={"font-bold text-white"}>그라비티</p>
+              <p className={"text-white"}>2022.05 ~ 2024.03 : 정보보안Unit</p>
+              <br />
+              <p className={"font-bold text-white"}>사이버루나</p>
+              <p className={"text-white"}>2024.03 ~ 2024.12 : 그레이 해커</p>
+            </div>
+            <div className={"rounded-lg bg-black bg-opacity-50 p-4 shadow-md"}>
+              <p className={"font-bold text-white"}>프로그래밍</p>
+              <br />
+              <p className={"font-bold text-white"}>사이버루나</p>
+              <p className={"text-white"}>
+                2018.08 ~ 2018.12 : 스마트 팜 제어 시스템
+              </p>
+              <br />
+              <p className={"text-white"}>
+                2019.01 ~ 2019.03 : 악성프로그램 탐지 프로그램
+              </p>
+              <br />
+
+              <p className={"text-white"}>
+                2021.03 ~ 2022.05 : 보안관제 분석 보조 시스템
+              </p>
+              <br />
+              <p className={"text-white"}>
+                2022.01 ~ 2022.03 : 정보보안 메일링 시스템
+              </p>
+
+              <p className={"text-white"}>
+                2022.05 ~ 2022.12 : EDR 분석 시스템
+              </p>
+              <br />
+              <p className={"text-white"}>
+                2023.01 ~ 2023.06 : SSL/TLS 인증서 관리 시스템
+              </p>
+              <p className={"text-white"}>
+                2023.06 ~ 2025.12 : 미 정부기관 AI 질병 탐지 시스템
+              </p>
+            </div>
+          </div>
+          <div
+            className={"w-full rounded-lg bg-black bg-opacity-50 p-4 shadow-md"}
+          >
+            <p className={"font-bold text-white"}>
+              처음 만나는 언덕 뒤의 거대한 언덕을 항상 생각하라.
+            </p>
+          </div>
+        </div>
       </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div
+        className={"h-full w-full bg-gradient-to-b from-black to-purple-950"}
+      >
+        s
       </div>
-    </main>
+    </div>
   );
 }
