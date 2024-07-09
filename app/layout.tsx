6cc8c30c3162bc0f504d75e5753a5c7b8ddf92f5
teamk <team.k0konutz@gmail.com>
2024-07-09 20:43:49 +0900

    init

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,12 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Noto_Sans } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Noto_Sans({
+  weight: "400",
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
   title: "사이버루나 - 이병호",
